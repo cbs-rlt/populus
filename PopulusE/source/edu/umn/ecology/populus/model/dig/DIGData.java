d377c4631faff94ca64d23a71b2eeb1c409b7be9
lars <lars@larsroe.com>
2014-08-01 23:45:50 -0400

    formatting
    Signed-off-by: lars <lars@larsroe.com>

--- PopulusE/source/edu/umn/ecology/populus/model/dig/DIGData.java
+++ PopulusE/source/edu/umn/ecology/populus/model/dig/DIGData.java
@@ -2,25 +2,22 @@ package edu.umn.ecology.populus.model.dig;
 import java.io.Serializable;
 
 class DIGData extends Object implements Serializable {
-   /**
-	 * 
-	 */
 	private static final long serialVersionUID = 2469088803649153178L;
-public static int selection;
-   public static double gensPF;
-   public static boolean isContinuous;
-   protected double rPF;
-   protected double nOPF;
-   
-   //gensPF.getCurrentValue(),lambdaPF.getCurrentValue(), n0PF.getCurrentValue(), rPF.getCurrentValue()
-   protected double lambdaPF;
-   
-   DIGData( boolean a, int b, double c, double d, double e, double f ) {
-      isContinuous = a;
-      selection = b;
-      gensPF = c;
-      lambdaPF = d;
-      nOPF = e;
-      rPF = f;
-   }
+	
+	//TODO - why are these static??
+	public static int selection;
+	public static double gensPF;
+	public static boolean isContinuous;
+	double rPF;
+	double nOPF;
+	double lambdaPF;
+
+	DIGData(boolean a, int b, double c, double d, double e, double f) {
+		isContinuous = a;
+		selection = b;
+		gensPF = c;
+		lambdaPF = d;
+		nOPF = e;
+		rPF = f;
+	}
 }
