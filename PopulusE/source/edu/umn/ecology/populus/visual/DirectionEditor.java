d702c8614a6459285b786eabbdeb99aacbcfa429
lars <lars@larsroe.com>
2014-08-02 22:17:40 -0400

    Remove last reference to JBCL.
    Signed-off-by: lars <lars@larsroe.com>

--- PopulusE/source/edu/umn/ecology/populus/visual/DirectionEditor.java
+++ PopulusE/source/edu/umn/ecology/populus/visual/DirectionEditor.java
@@ -1,19 +1,46 @@
 package edu.umn.ecology.populus.visual;
-import com.borland.jbcl.editors.IntegerTagEditor;
 
-/** Use to define the direction the HTMLLabel will point.*/
+import java.beans.PropertyEditorSupport;
+import java.util.ArrayList;
 
-public class DirectionEditor extends IntegerTagEditor {
+/** Use to define the direction the HTMLLabel will point. */
 
-   public DirectionEditor() {
-      super( new int[] {
-   HTMLLabel.NORMAL, HTMLLabel.DOWN_TO_UP, HTMLLabel.UPSIDEDOWN,
-   HTMLLabel.UP_TO_DOWN
-}, new String[] {
-   "Normal", "Down to Up (90deg CC)", "Upside-Down", "Up to Down (270deg CC)"
-}, new String[] {
-   "HTMLLabel.NORMAL", "HTMLLabel.DOWN_TO_UP", "HTMLLabel.UPSIDEDOWN",
-   "HTMLLabel.UP_TO_DOWN"
-} );
-   }
+public class DirectionEditor extends PropertyEditorSupport {
+	private ArrayList<Integer> values;
+	private ArrayList<String> names;
+	
+	@SuppressWarnings("serial")
+	public DirectionEditor() {
+		values = new ArrayList<Integer>() {{
+			add(HTMLLabel.NORMAL);
+			add(HTMLLabel.DOWN_TO_UP);
+			add(HTMLLabel.UPSIDEDOWN);
+			add(HTMLLabel.UP_TO_DOWN);
+		}};
+		names = new ArrayList<String>() {{
+			add("Normal");
+			add("Down to Up (90deg CC)");
+			add("Upside-Down");
+			add("Up to Down (270deg CC)");
+		}};
+	}
+	
+	public String getAsText() {
+		int idx = values.indexOf(getValue());
+		if (idx >= 0)
+			return names.get(idx);
+		return null;
+	}
+	
+	public void SetAsText(String text) throws java.lang.IllegalArgumentException {
+		int idx = names.indexOf(text);
+		if (idx < 0)
+			throw new java.lang.IllegalArgumentException(text);
+		setValue(values.get(idx));
+	}
+	
+	public String[] getTags() {
+		String s[] = new String[] {};
+		return names.toArray(s);
+	}
 }
