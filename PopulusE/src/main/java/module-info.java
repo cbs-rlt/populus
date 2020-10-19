8943665517fb4b2905cb6cf10cca548ee05e56ca
Lars Roe <lars@larsroe.com>
2020-10-19 15:18:25 -0500

    Add module-info.java to project. Removed a sample class in the top-level directory in acrobat.jar, which is bad for modules.

---
+++ PopulusE/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+open module edu.umn.ecology.populus {
+    requires java.desktop; //Swing GUI
+    requires jcchart401K;
+    requires jcommon;
+    requires Jama;
+    requires jfreechart;
+    requires acrobat;
+}
