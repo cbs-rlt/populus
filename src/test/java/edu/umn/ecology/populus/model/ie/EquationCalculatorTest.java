26865cfb5fbd1c1b9fbf04ecde2f114f5f7fc813
Lars Roe <lars@larsroe.com>
2020-11-04 20:51:00 -0600

    Equation Calculator test
    Signed-off-by: Lars Roe <lars@larsroe.com>

--- src/test/java/edu/umn/ecology/populus/model/ie/EquationCalculatorTest.java
+++ src/test/java/edu/umn/ecology/populus/model/ie/EquationCalculatorTest.java
@@ -1,4 +1,23 @@
+package edu.umn.ecology.populus.model.ie;
+
+import org.junit.jupiter.api.Test;
+import java.util.Hashtable;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class EquationCalculatorTest {
-  
+
+    @Test
+    void testEquationConstants() throws IEException {
+        String[] equations = new String[] {"5*1.2 + k", "E^2+7.0"};
+        Hashtable<String, Double> consts = new Hashtable<>();
+        consts.put("k", -6.0);
+        double[] initState = new double[] {0.0, 0.0};
+
+        EquationCalculator ec = new EquationCalculator(equations, consts, false);
+        double[] sols = ec.calculateValues(initState, 0.0);
+        assertEquals(2, sols.length);
+        assertEquals(0.0, sols[0], 0.001);
+        assertEquals(Math.E*Math.E + 7.0, sols[1], 0.001);
+    }
 }
