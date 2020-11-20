b8f22b961c68b2a4122ed618100b4f6088aae9e7
Lars Roe <lars@larsroe.com>
2020-11-20 10:10:11 -0600

    Remove modules in top level. It causes a lot of headaches, and this project is not meant to be consumed by something else.

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-open module edu.umn.ecology.populus {
-    requires java.desktop; //Swing GUI
-    requires jcchart401K;
-    requires jcommon;
-    requires jama;
-    requires jfreechart;
-    requires acrobat;
-}
